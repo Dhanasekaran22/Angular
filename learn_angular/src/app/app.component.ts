5be86c640eb00adc18b2d6d60522f033bc3088e7
Dhanasekaran22 <dhanasekaran2622@gmail.com>
2024-10-25 18:10:47 +0530

    daily task updated

--- learn_angular/src/app/app.component.ts
+++ learn_angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +9,22 @@ export class AppComponent {
   title = 'learn_angular';
 
   //ngOnChanges()
-  message:string="From Parent";
+  message: string = "From Parent";
+
+
+  //ngDoCheck()
+  parentTaskList = [
+    { title: 'task1', completed: false },
+    { title: 'task2', completed: false },
+  ];
+
+
+  markTaskAsCompleted(index: number) {
+    this.parentTaskList[index].completed = true;         // Angular wont see this Change
+    console.log("clicked")                      // In a 'component' angular didn't see the change but when we console and see, the changes are made 
+                                                // but in angular we create new reference to make the angular note that it was changed
+
+    
+    
+  }
 }
--- learn_angular/src/app/life-cycle-hooks/life-cycle-hooks.component.ts
+++ learn_angular/src/app/life-cycle-hooks/life-cycle-hooks.component.ts
@@ -1,32 +1,42 @@
-import { Component,Input,OnChanges, SimpleChanges,OnInit } from '@angular/core';
+import { Component,Input,OnChanges, SimpleChanges,OnInit,DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-life-cycle-hooks',
   templateUrl: './life-cycle-hooks.component.html',
   styleUrls: ['./life-cycle-hooks.component.css']
 })
-export class LifeCycleHooksComponent implements OnChanges,OnInit{
+export class LifeCycleHooksComponent implements OnChanges,OnInit,DoCheck{
   
   //ngOnChanges()
 
   @Input() msgFromApp="";
+tasks: any;
 
   ngOnChanges(changes: SimpleChanges): void {
     console.log("ngOnChanges Called!");
 
-    // if(!changes['msgFromApp'].isFirstChange()){
-    //   console.log("You are modifying the original value");
-    //   console.log(changes['msgFromApp'].currentValue);
-    // }
+    if(!changes['msgFromApp'].isFirstChange()){
+      console.log("You are modifying the original value");
+      console.log(changes['msgFromApp'].currentValue);
+    }
   }
 
   //ngOnInit
   
   ngOnInit(): void {
     console.log("ngOnInit Called");
-    this.msgFromApp='initialize through ngOnInit()'     //clear example of ngOninit
-    
+    this.msgFromApp='initialize through ngOnInit()'     //clear example of ngOnInit()
+  }
 
-    
+  @Input() taskItems:any[]=[];
+
+  previousTaskItems: any[]=[];
+
+  ngDoCheck(): void {
+    if(this.taskItems!==this.previousTaskItems){
+      console.log("Tasks have Changed");
+      this.previousTaskItems=[...this.taskItems]     // new reference(spread operator) was created and angular note the changes
+      
+    }
   }
 }
